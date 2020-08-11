06f0d6c713ad0bab2efee51bc3fb48e683773345
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-11 11:06:03 -0300

    initial commit

--- Project4-SwiftUI-BetterRest/ContentView.swift
+++ Project4-SwiftUI-BetterRest/ContentView.swift
@@ -9,8 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        Stepper(value: $sleepAmount) {
+            Text("\(sleepAmount) hours")
+        }
     }
 }
 
